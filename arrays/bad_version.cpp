a01b3b6d5417347550210dde655c32cca5baa968
ElizabethSvit <lizasvit@gmail.com>
2018-06-07 11:43:37 -0700

    link added

--- arrays/bad_version.cpp
+++ arrays/bad_version.cpp
@@ -1,5 +1,5 @@
 /*
-Leetcode from
+Leetcode from https://leetcode.com/problems/first-bad-version/description/
 
 You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
