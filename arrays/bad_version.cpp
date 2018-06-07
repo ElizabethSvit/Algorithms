066b68ef060038e5c87007cbaf38d5025ee31604
ElizabethSvit <lizasvit@gmail.com>
2018-06-07 11:42:22 -0700

    Find first bad version

---
+++ arrays/bad_version.cpp
@@ -0,0 +1,48 @@
+/*
+Leetcode from
+
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version. 
+
+
+*/
+
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int start = 0;
+        int end = n;
+        int mid;
+        
+        int first_bad_vers = -1;
+        
+        while (start < end) {
+            mid = (end - start) / 2 + start;
+            // cout << start << " " << end;
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                first_bad_vers = mid;
+                end = mid;
+            }
+        }
+        return first_bad_vers == -1 ? n : first_bad_vers;
+    }
+};
