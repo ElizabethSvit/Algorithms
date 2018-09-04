ca11c1086402b051c84c743c4c3a3ed8420f9ec4
Elizaveta <lizasvit@gmail.com>
2018-09-04 20:27:01 +0300

    Course schedule II

---
+++ arrays/course_schedule_ii.cpp
@@ -0,0 +1,50 @@
+/*
+Leetcode task from https://leetcode.com/problems/course-schedule-ii/description/
+
+Pseudo code:
+
+make adjacency matrix for all pairs
+
+put nodes in the queue and in degrees
+take from queue and put all the neighbours in queue
+when in degree is 0 put in the result
+
+*/
+
+class Solution {
+public:
+    
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<int> indegrees(numCourses, 0);
+        
+        queue<int> nodes;
+        vector<int> result;
+        
+        // fill the graph and indegrees
+        for (const auto &pair: prerequisites) {
+            graph[pair.second].push_back(pair.first);
+            ++indegrees[pair.first];
+        }
+        
+        // fill up the queue with 0 indegrees
+        for (int i = 0; i < indegrees.size(); ++i) {
+            if (indegrees[i] == 0) nodes.push(indegrees[i]);
+        }
+        
+        while (!nodes.empty()) {
+            int node = nodes.front();
+            nodes.pop();
+            result.push_back(node);
+            
+            for (int neighbour: graph[node]) {
+                 
+                indegrees[neighbour]--;
+                if (indegrees[neighbour] == 0) nodes.push(neighbour);
+            }
+        }
+        
+        return result.size() ? result : vector<int>();
+    }
+};
+
