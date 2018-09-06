91c5a528f2bc7785302f4d08a4c38499222ab57a
Elizaveta <lizasvit@gmail.com>
2018-09-06 16:26:00 +0300

    Encode and decode URL to a smaller version

---
+++ strings/tiny_url.cpp
@@ -0,0 +1,47 @@
+/*
+Solution from https://leetcode.com/discuss/interview-question/124658/Design-a-URL-Shortener-(-TinyURL-)-System/
+
+Code solution (base 62 numerical system):
+*/
+
+string idToShortURL(long int n)
+{
+    // Map to store 62 possible characters
+    char map[] = "abcdefghijklmnopqrstuvwxyzABCDEF"
+                 "GHIJKLMNOPQRSTUVWXYZ0123456789";
+  
+    string shorturl;
+  
+    // Convert given integer id to a base 62 number
+    while (n)
+    {
+        shorturl.push_back(map[n%62]);
+        n = n/62;
+    }
+  
+    // Reverse shortURL to complete base conversion
+    reverse(shorturl.begin(), shorturl.end());
+  
+    return shorturl;
+}
+  
+// Function to get integer ID back from a short url
+long int shortURLtoID(string shortURL)
+{
+    long int id = 0; // initialize result
+  
+    // A simple base conversion logic
+    for (int i=0; i < shortURL.length(); i++)
+    {
+        if ('a' <= shortURL[i] && shortURL[i] <= 'z')
+          id = id*62 + shortURL[i] - 'a';
+        if ('A' <= shortURL[i] && shortURL[i] <= 'Z')
+          id = id*62 + shortURL[i] - 'A' + 26;
+        if ('0' <= shortURL[i] && shortURL[i] <= '9')
+          id = id*62 + shortURL[i] - '0' + 52;
+    }
+    return id;
+}
+
+
+
