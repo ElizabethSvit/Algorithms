d9abb3528612e64d91012b7adcd87b54e841bebf
Elizaveta <lizasvit@gmail.com>
2018-09-21 13:24:04 +0300

    Meeting rooms easy version and counting the number

--- arrays/meeting_rooms.cpp
+++ arrays/meeting_rooms.cpp
@@ -1,5 +1,5 @@
 /*
- Meeting rooms
+ Meeting rooms I
  
  Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
  determine if a person could attend all meetings.
@@ -10,14 +10,36 @@
  Understand the problem:
  The problem looks very similar to the merge interval and insert intervals.
  So the idea is still the same: first sort the intervals according to the start times, then check if there is any overlap.
+ 
+ Meeting rooms II
+ 
+ Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ find the minimum number of conference rooms required.
+ 
+ For example,
+ Given [[0, 30],[5, 10],[15, 20]], return 2.
+ 
+ Pseudo code:
+ 
+ init stack
+ put first interval
+ 
+ while not empty:
+    if (top.end < current.start)
+        while top.end < current.start:
+            stack pop
+        counter++
+    else:
+        put current in stack and counter++
  */
 
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-bool meeting_rooms(vector<pair<int, int>>& intervals) {
+bool meeting_rooms_i(vector<pair<int, int>>& intervals) {
     sort(intervals.begin(), intervals.end());
     for (int i = 0; i < intervals.size(); ++i) {
         if (i != intervals.size() - 1 && intervals[i].second > intervals[i+1].first) {
@@ -27,9 +49,29 @@ bool meeting_rooms(vector<pair<int, int>>& intervals) {
     return true;
 }
 
+int meeting_rooms_ii(vector<pair<int, int>>& intervals) {
+    if (intervals.size() < 2) return 1;
+    
+    sort(intervals.begin(), intervals.end());
+    stack<pair<int, int>> rooms;
+    int counter = 0;
+    
+    for (int i = 0; i < intervals.size(); ++i) {
+        if (rooms.empty() || rooms.top().second > intervals[i].first) {
+            rooms.push(intervals[i]);
+        } else if (rooms.top().second < intervals[i].first) {
+            while (!rooms.empty() && rooms.top().second < intervals[i].first) {
+                rooms.pop();
+            }
+            counter++;
+        }
+    }
+    return counter + static_cast<int>(rooms.size());
+}
+
 int main(int argc, const char * argv[]) {
     vector<pair<int, int>> intervals = {{0, 4}, {5, 10}, {15, 20}};
-    cout << meeting_rooms(intervals);
+    cout << meeting_rooms_ii(intervals);
     
     return 0;
 }
